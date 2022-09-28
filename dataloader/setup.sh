ee4519f25a54a2554c120fb17bd21e43224a31b9
Silvia Chen <silviaxchen@gmail.com>
2022-09-27 20:33:40 -0400

    added dataloader

---
+++ dataloader/setup.sh
@@ -0,0 +1,31 @@
+
+#!/bin/bash
+# bash script to set up dataset and dataloader 
+
+# install dependencies
+pip3 install -r requirements.txt
+
+# download the musemorphose-main folder
+gdown --fuzzy https://drive.google.com/file/d/1kvjs-XDdCOpyTFZ0UUi5SNVNldeStTna/view?usp=sharing
+unzip MuseMorphose-main.zip
+rm MuseMorphose-main.zip
+
+# download the processed dataset
+# current dataset: remi_dataset_0926_updated.tar.gz
+gdown --fuzzy https://drive.google.com/file/d/1lMOY5ocnYyaW9ypgIQ3_obpZPfpP5VMM/view?usp=sharing
+tar xzvf remi_dataset_0926_updated.tar.gz
+mv content/remi_dataset/ MuseMorphose-main/
+rm -r content/
+rm remi_dataset_0926_updated.tar.gz
+
+# remove original remi_vocab.pkl and replace with ours
+rm MuseMorphose-main/pickles/remi_vocab.pkl
+gdown --fuzzy https://drive.google.com/file/d/15GIjOpW4biMXanUDcE8W7ZHQ0Jcyph7E/view?usp=sharing
+mv remi_vocab.pkl MuseMorphose-main/pickles/
+
+# download no_events_fn.txt where we store files that we do not consider 
+# because they have no events between some consecutive bars
+gdown --fuzzy https://drive.google.com/file/d/1Fc1kxMTH-A4XIAh8lLvIf7MKuDMFd8Gp/view?usp=sharing
+
+# download the splitted datasets with labels
+gdown --folder https://drive.google.com/drive/folders/13UbMp_Q0PvnPsVNs7Aw4RWeiwT5cRatl?usp=sharing
