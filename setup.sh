d5be6a4902743ccdb943e6b4cc8cd33879e414f4
Silvia Chen <44254020+silvia0v0@users.noreply.github.com>
2022-11-16 15:04:01 -0500

    Update setup.sh
    updated to download new converted pretrained weights

--- setup.sh
+++ setup.sh
@@ -8,7 +8,9 @@ echo "download musemorphose pretrained weights"
 wget -O musemorphose_pretrained_weights.pt https://zenodo.org/record/5119525/files/musemorphose_pretrained_weights.pt?download=1
 
 echo "download musemorphose converted weights"
-gdown --fuzzy https://drive.google.com/file/d/15v5tRmOueYUg0pxrdX1i29KrxrzjjA4v/view?usp=sharing
+# gdown --fuzzy https://drive.google.com/file/d/15v5tRmOueYUg0pxrdX1i29KrxrzjjA4v/view?usp=sharing
+gdown --fuzzy https://drive.google.com/file/d/1TbahcwKvj3i-_GRvRoZSs4aGTd_mIItg/view?usp=sharing  # updated weights
+mv converted_pretrained_weights_updated.pt converted_pretrained_weights.pt  # rename to original name
 
 echo "setup dataloader"
 sh dataloader/setup.sh
