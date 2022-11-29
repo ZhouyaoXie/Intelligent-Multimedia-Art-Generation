28e40cc158fa8e9b76dafd2044a79ca7d4ee2294
Silvia Chen <44254020+silvia0v0@users.noreply.github.com>
2022-11-28 19:15:08 -0500

    Update train_new.py
    update to include weight annealing

--- model/train_new.py
+++ model/train_new.py
@@ -16,6 +16,8 @@ from config.text_config import text_args
 from model.inference import MusicCLIPInfer
 from .contrastive_loss import ContrastiveLoss
 
+from tqdm import tqdm
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 print("the set device is ", device)
@@ -34,6 +36,8 @@ config_path = "config/default.yaml"
 
 # contrastive loss if music and text match 
 POSITIVE = 1
+music_config_global = yaml.load(open(config_path, 'r'), Loader=yaml.FullLoader)
+trained_steps = music_config_global['training']['trained_steps']
 
 # music_config = yaml.load(open(config_path, 'r'), Loader=yaml.FullLoader)
 # model_out_path = music_config['training']['output_path']
@@ -52,6 +56,9 @@ def _train(music_config, text_args):
     bs = music_config["data"]["batch_size"]
     epochs = music_config['training']['max_epochs']
     lr = music_config['training']['max_lr']
+    min_lr = music_config['training']['min_lr']
+    lr_decay_steps = music_config['training']['lr_decay_steps']
+    lr_warmup_steps = music_config['training']['lr_warmup_steps']
     
     if not os.path.exists(model_out_path):
         os.makedirs(model_out_path)
@@ -65,6 +72,9 @@ def _train(music_config, text_args):
 
     model.to(device)
     optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay = 5e-4)
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(
+      optimizer, lr_decay_steps, eta_min=min_lr
+    )  # added (same as musemorphose)
     c_loss = ContrastiveLoss(bs)
 
     model.zero_grad()
@@ -73,7 +83,9 @@ def _train(music_config, text_args):
     model.train()
     for epoch in range(epochs):
         print("Starting epoch ", epoch)
-        for batch_idx, batch_samples in enumerate(train_dloader):
+        for batch_idx, batch_samples in tqdm(enumerate(train_dloader)):
+            if batch_idx > 0.1 * len(train_dloader):
+                break
             model.zero_grad()
             batch_enc_inp = batch_samples['enc_input'].permute(2, 0, 1).to(device)
             batch_dec_inp = batch_samples['dec_input'].permute(1, 0).to(device)
@@ -84,7 +96,10 @@ def _train(music_config, text_args):
             # text = torch.Tensor(batch_samples['text_label']).permute(1, 0).to(device)
             text = batch_samples['text_label']
             y = batch_samples['pos'].float().to(device)
-
+            
+            global trained_steps
+            trained_steps += 1
+            
             lang_feats, music_feats, pooled_output, lang_attention_mask = model(
                 text,
                 batch_enc_inp, 
@@ -98,11 +113,19 @@ def _train(music_config, text_args):
             )
             # print('y', y)
             loss = c_loss(music_feats, lang_feats, y)
+            # anneal learning rate, added
+            if trained_steps < lr_warmup_steps:
+                curr_lr = lr * trained_steps / lr_warmup_steps
+                optimizer.param_groups[0]['lr'] = curr_lr
+            else:
+                scheduler.step(trained_steps - lr_warmup_steps)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            print("loss", loss.item())
+#             print("loss", loss.item())
             #training accuracy
+            if batch_idx % 1000 == 0:
+                print("batch_idx:", batch_idx, "loss:", loss.item())
 
         # save model checkpoint after every epoch
         torch.save({'epoch': epoch,
