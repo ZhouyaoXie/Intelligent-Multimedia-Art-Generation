4b80002bfd6d09a96dae71eb697829e8c4038eff
ZhouyaoXie <zhouyaox@andrew.cmu.edu>
2022-12-08 16:10:35 -0500

    fix musicCLIPInference forward

--- model/inference.py
+++ model/inference.py
@@ -180,7 +180,7 @@ class MusicCLIPInfer(torch.nn.Module):
         # Run language layers from pretrianed bert
         lang_feats = self.model.bert(lang_feats, lang_attention_mask)
         print("lang_feats shape:", lang_feats.shape)
-        lang_feats = self.pooler(lang_feats)
+        lang_feats = self.model.pooler(lang_feats)
         print("lang_feats shape after bertpooler:", lang_feats.shape)
 
         # Extend the music emb output to text emb output
